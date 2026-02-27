f0234309d8f205540fe29b376655071e8116280b
woustachemaxd <125763534+woustachemaxd@users.noreply.github.com>
2026-02-27 21:32:47 +0530

    Submit: alin-verma — source/alin-verma/src/components/ui/date-picker.tsx

--- source/alin-verma/src/components/ui/date-picker.tsx
+++ source/alin-verma/src/components/ui/date-picker.tsx
@@ -52,7 +52,6 @@ export function DatePicker({
               onDateChange(format(day, "yyyy-MM-dd"))
             }
           }}
-          initialFocus
         />
       </PopoverContent>
     </Popover>
