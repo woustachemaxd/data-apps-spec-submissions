558fc08852f31f44cbbecb9e1b8ce243ea53976f
woustachemaxd <125763534+woustachemaxd@users.noreply.github.com>
2026-02-27 21:15:15 +0530

    Submit: rohith-nagula — submission/rohith-nagula/index.html

---
+++ source/rohith-nagula/src/App.tsx
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Snowcone Starter</title>
+    <script type="module" crossorigin src="/Users/accel/AppData/Local/Programs/Git/submission/rohith-nagula/assets/index-C41DI88U.js"></script>
+    <link rel="stylesheet" crossorigin href="/Users/accel/AppData/Local/Programs/Git/submission/rohith-nagula/assets/index-z3tQYh00.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
